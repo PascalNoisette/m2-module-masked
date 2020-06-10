b6674b498cea6d21bc8f2974e888f68ffab45e5c
Pascal Noisette <pascal.noisette@smile.fr>
2020-06-10 18:26:44 +0200

    first commit

---
+++ Model/MaskedQuoteIdToQuoteId.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Netpascal\Masked\Model;
+
+/**
+ * Converts quote id to the masked quote id
+ * @api
+ * @since 101.1.0
+ */
+class MaskedQuoteIdToQuoteId extends \Magento\Quote\Model\MaskedQuoteIdToQuoteId implements \Netpascal\Masked\Api\MaskedQuoteIdToQuoteIdInterface
+{
+    
+    
+}
---
+++ Model/QuoteIdToMaskedQuoteId.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Netpascal\Masked\Model;
+
+/**
+ * Converts quote id to the masked quote id
+ * @api
+ * @since 101.1.0
+ */
+class QuoteIdToMaskedQuoteId extends \Magento\Quote\Model\QuoteIdToMaskedQuoteId implements \Netpascal\Masked\Api\QuoteIdToMaskedQuoteIdInterface
+{
+    
+    
+}
