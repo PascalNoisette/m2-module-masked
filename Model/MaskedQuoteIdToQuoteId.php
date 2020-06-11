edc56f9b7471b4dd45b6b31d49cd185f9401983c
Pascal Noisette <pascal.noisette@smile.fr>
2020-06-11 09:14:55 +0200

    add graphql interface

--- Model/MaskedQuoteIdToQuoteId.php
+++ Model/MaskedQuoteIdToQuoteId.php
@@ -7,13 +7,36 @@ declare(strict_types=1);
 
 namespace Netpascal\Masked\Model;
 
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 /**
  * Converts quote id to the masked quote id
  * @api
  * @since 101.1.0
  */
-class MaskedQuoteIdToQuoteId extends \Magento\Quote\Model\MaskedQuoteIdToQuoteId implements \Netpascal\Masked\Api\MaskedQuoteIdToQuoteIdInterface
+class MaskedQuoteIdToQuoteId extends \Magento\Quote\Model\MaskedQuoteIdToQuoteId implements \Magento\Framework\GraphQl\Query\ResolverInterface, \Netpascal\Masked\Api\MaskedQuoteIdToQuoteIdInterface
 {
     
-    
+    /**
+     * Fetches the data from persistence models and format it according to the GraphQL schema.
+     *
+     * @param \Magento\Framework\GraphQl\Config\Element\Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @throws \Exception
+     * @return mixed|Value
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        return $this->execute($args["identifier"]);
+    }    
 }
--- Model/QuoteIdToMaskedQuoteId.php
+++ Model/QuoteIdToMaskedQuoteId.php
@@ -7,13 +7,36 @@ declare(strict_types=1);
 
 namespace Netpascal\Masked\Model;
 
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 /**
  * Converts quote id to the masked quote id
  * @api
  * @since 101.1.0
  */
-class QuoteIdToMaskedQuoteId extends \Magento\Quote\Model\QuoteIdToMaskedQuoteId implements \Netpascal\Masked\Api\QuoteIdToMaskedQuoteIdInterface
+class QuoteIdToMaskedQuoteId extends \Magento\Quote\Model\QuoteIdToMaskedQuoteId implements \Magento\Framework\GraphQl\Query\ResolverInterface, \Netpascal\Masked\Api\QuoteIdToMaskedQuoteIdInterface
 {
     
-    
+    /**
+     * Fetches the data from persistence models and format it according to the GraphQL schema.
+     *
+     * @param \Magento\Framework\GraphQl\Config\Element\Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @throws \Exception
+     * @return mixed|Value
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        return $this->execute($args["id"]);
+    }
 }
